cb6c4f1e0cb139ba557d274845ce3b09a083cbac
EngineerUmarFarooq <149157063+EngineerUmarFarooq@users.noreply.github.com>
2024-03-11 13:28:37 +0500

    Update flutter_uni.dart

--- flutter_uni.dart
+++ flutter_uni.dart
@@ -1,3 +1,94 @@
-int calculate() {
-  return 6 * 7;
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoginApp());
+}
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: LoginForm(),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              labelText: 'Email',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter your email';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 16.0),
+          TextFormField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 32.0),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                // Perform login functionality
+                // For now, we'll just print the email and password
+                print('Email: ${_emailController.text}');
+                print('Password: ${_passwordController.text}');
+              }
+            },
+            child: Text('Login'),
+          ),
+        ],
+      ),
+    );
+  }
 }
